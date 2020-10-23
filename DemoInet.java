79c4a82c5d5d5aa444b6241f07723b37f56f9007
Manavi Thorve <manavithorve09@gmail.com>
2020-10-23 15:43:52 +0530

    Imported Specific(Inetaddress) Class

--- DemoInet.java
+++ DemoInet.java
@@ -1,22 +1,20 @@
 //InetAddress with its factory methods:
-import java.net.*;
+import java.net.InetAddress;
 public class DemoInet
 {
 	public static void main(String args[]) throws UnknownHostException
 	{
 		InetAddress addr=InetAddress.getLocalHost();
 		System.out.println(addr);
-	addr=InetAddress.getByName("google.com");
-	InetAddress sw[]=InetAddress.getAllByName("www.google.com");
-	for(int i=0;i<sw.length;i++)
-	{
-		System.out.println(sw[i]);
-	}
-	String name=addr.getHostName();
-	System.out.println(name);
-	String address=addr.getHostAddress();
-	System.out.println(address);
-
-	
+		addr=InetAddress.getByName("google.com");
+		InetAddress sw[]=InetAddress.getAllByName("www.google.com");
+		for(int i=0;i<sw.length;i++)
+		{
+			System.out.println(sw[i]);
+		}
+		String name=addr.getHostName();
+		System.out.println(name);
+		String address=addr.getHostAddress();
+		System.out.println(address);
 	}
 }
